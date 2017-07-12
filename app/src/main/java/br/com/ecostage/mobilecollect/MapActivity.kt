60e270c933a3b61fec9c4596c250d6730943671a
Caio Maia <caaiomaia@gmail.com>
2017-07-12 01:36:37 -0300

    Fixed point in map, add new ui settings, simplify code (wip)

--- app/src/main/java/br/com/ecostage/mobilecollect/MapActivity.kt
+++ app/src/main/java/br/com/ecostage/mobilecollect/MapActivity.kt
@@ -2,127 +2,55 @@ package br.com.ecostage.mobilecollect
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
+import android.support.v4.app.FragmentActivity
 import android.support.v4.content.ContextCompat
-import android.support.v7.app.AppCompatActivity
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.common.api.CommonStatusCodes
-import com.google.android.gms.common.api.ResolvableApiException
-import com.google.android.gms.location.*
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback {
-    val LOCATION_REQUEST_CODE = 0
-    lateinit var map: GoogleMap
-    lateinit var mLocationRequest: LocationRequest
-    lateinit var mLocationCallback: LocationCallback
-    private var mFusedLocationClient: FusedLocationProviderClient? = null
-    private val REQUEST_CHECK_SETTINGS = 1
+class MapActivity : FragmentActivity(), OnMapReadyCallback {
+
+    private val MAP_PERMISSION_REQUEST_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
-        createLocationRequest()
+        if (!canAccessLocation()) requestPermissions()
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
-        mLocationCallback = object : LocationCallback() {
-            override fun onLocationResult(locationResult: LocationResult) {
-                for (location in locationResult.locations) {
-                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude, location.longitude), 14.0f))
-                }
-            }
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        startLocationUpdates()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        stopLocationUpdates()
-    }
-
-    private fun stopLocationUpdates() {
-        mFusedLocationClient?.removeLocationUpdates(mLocationCallback)
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun startLocationUpdates() {
-        if (canAccessLocation()) {
-            mFusedLocationClient?.requestLocationUpdates(mLocationRequest,
-                    mLocationCallback,
-                    null)
-        }
     }
 
     @SuppressLint("MissingPermission")
     override fun onMapReady(map: GoogleMap) {
-        this.map = map
+        setupUiSettings(map)
+
         map.mapType = GoogleMap.MAP_TYPE_SATELLITE
+
         if (canAccessLocation()) {
             map.isMyLocationEnabled = true
         } else {
-            ActivityCompat.requestPermissions(this,
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                    LOCATION_REQUEST_CODE)
+            requestPermissions()
         }
     }
 
-    @SuppressLint("MissingPermission")
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<String>, grantResults: IntArray) {
-        when (requestCode) {
-            LOCATION_REQUEST_CODE -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    map.isMyLocationEnabled = true
-                }
-            }
-        }
+    private fun setupUiSettings(map: GoogleMap) {
+        val uiSettings = map.uiSettings
+
+        uiSettings.isScrollGesturesEnabled = false
+        uiSettings.isZoomControlsEnabled = true
+        uiSettings.isZoomGesturesEnabled = true
     }
 
-    protected fun createLocationRequest() {
-        mLocationRequest = LocationRequest()
-        mLocationRequest.interval = 5000
-        mLocationRequest.fastestInterval = 5000
-        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-
-        val builder = LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest)
-
-        val client = LocationServices.getSettingsClient(this)
-        val task = client.checkLocationSettings(builder.build())
-
-        task.addOnFailureListener(this) { e ->
-            val statusCode = (e as ApiException).statusCode
-            when (statusCode) {
-                CommonStatusCodes.RESOLUTION_REQUIRED ->
-                    try {
-                        val resolvable = e as ResolvableApiException
-                        resolvable.startResolutionForResult(this@MapActivity,
-                                REQUEST_CHECK_SETTINGS)
-                    } catch (sendEx: IntentSender.SendIntentException) {
-
-                    }
-
-                LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
-                }
-            }
-        }
+    fun requestPermissions() {
+        ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION),
+                MAP_PERMISSION_REQUEST_CODE)
     }
 
-    protected fun canAccessLocation(): Boolean {
-        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-    }
+    fun canAccessLocation(): Boolean = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
 }
