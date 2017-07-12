984af9ba22478fbbc96d8f47972021ae7d183f5f
André Maia <andrenmaia@gmail.com>
2017-07-12 10:37:48 -0300

    Executes sign in with google
    - Some refactoring and improvements was done

--- app/src/main/java/br/com/ecostage/mobilecollect/LoginActivity.kt
+++ app/src/main/java/br/com/ecostage/mobilecollect/LoginActivity.kt
@@ -3,35 +3,40 @@ package br.com.ecostage.mobilecollect
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.annotation.TargetApi
-import android.app.LoaderManager.LoaderCallbacks
 import android.content.Intent
-import android.content.Loader
-import android.content.SharedPreferences
-import android.preference.PreferenceManager
-import android.database.Cursor
 import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.TextView
 import android.widget.Toast
-import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 
 /**
- * A login screen that offers login via email/password.
+ * A login screen that offers login via email/password and google.
  */
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener {
+
     lateinit private var mAuthListener: FirebaseAuth.AuthStateListener
+
+    private val TAG = "LoginActivity"
+    private val RC_SIGN_IN = 9001
+
+    private var mGoogleApiClient: GoogleApiClient? = null
     private var mAuth: FirebaseAuth? = null
-    private var prefs: SharedPreferences? = null
 
     override fun onStart() {
         super.onStart()
@@ -44,36 +49,57 @@ class LoginActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        mAuth = FirebaseAuth.getInstance()
-
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
 
-        mAuthListener = object:FirebaseAuth.AuthStateListener {
-            override fun onAuthStateChanged(firebaseAuth:FirebaseAuth) {
-                val user = firebaseAuth.getCurrentUser()
-                if (user != null)
-                {
-                    val intent = Intent(this@LoginActivity, MapActivity::class.java)
-                    this@LoginActivity.startActivity(intent)
-                }
-                else
-                {
+        setupFirebaseAuth()
+        setupSignInWithEmail()
+        setupSignInWithGoogle()
+    }
 
-                }
+    private fun setupFirebaseAuth() {
+        mAuth = FirebaseAuth.getInstance()
+        mAuthListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+            val user = firebaseAuth.currentUser
+            if (user != null) {
+                val intent = Intent(this@LoginActivity, MapActivity::class.java)
+                this@LoginActivity.startActivity(intent)
             }
         }
+    }
 
+    private fun setupSignInWithGoogle() {
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build()
+
+        google_sign_in_button.setOnClickListener { attemptSignInWithGoogle() }
+
+    }
+
+    private fun setupSignInWithEmail() {
         password.setOnEditorActionListener(TextView.OnEditorActionListener { _, id, _ ->
             if (id == R.id.login || id == EditorInfo.IME_NULL) {
-                attemptLogin()
+                attemptSignInWithEmail()
                 return@OnEditorActionListener true
             }
             false
         })
 
-        email_sign_in_button.setOnClickListener { attemptLogin() }
+        email_sign_in_button.setOnClickListener { attemptSignInWithEmail() }
+    }
+
+    private fun attemptSignInWithGoogle() {
+        val signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient)
+        startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
     /**
@@ -81,7 +107,7 @@ class LoginActivity : AppCompatActivity() {
      * If there are form errors (invalid email, missing fields, etc.), the
      * errors are presented and no actual login attempt is made.
      */
-    private fun attemptLogin() {
+    private fun attemptSignInWithEmail() {
         // Reset errors.
         email.error = null
         password.error = null
@@ -120,17 +146,9 @@ class LoginActivity : AppCompatActivity() {
             // perform the user login attempt.
             showProgress(true)
             mAuth?.signInWithEmailAndPassword(emailStr, passwordStr)
-                ?.addOnCompleteListener(this, object:OnCompleteListener<AuthResult> {
-                    override fun onComplete(task:Task<AuthResult>) {
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-                        if (!task.isSuccessful()) {
-                            showProgress(false)
-                            Toast.makeText(this@LoginActivity, getString(R.string.auth_failed), Toast.LENGTH_SHORT).show()
-                        }
+                    ?.addOnCompleteListener(this) {
+                        handleOnCompleteSignIn(it)
                     }
-                })
         }
     }
 
@@ -180,5 +198,57 @@ class LoginActivity : AppCompatActivity() {
             login_progress.visibility = if (show) View.VISIBLE else View.GONE
             login_form.visibility = if (show) View.GONE else View.VISIBLE
         }
+    }
+
+    override fun onConnectionFailed(connectionResult: ConnectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult)
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result.isSuccess) {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = result.signInAccount
+                firebaseAuthWithGoogle(account)
+            }
+        }
+    }
+
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount?) {
+
+        if (acct == null){
+            Log.e(TAG, "firebaseAuthWithGoogle: acct is null")
+            return
+
+        }
+
+        Log.d(TAG, "firebaseAuthWithGoogle: ${acct.id}")
+
+        showProgress(true)
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        mAuth?.signInWithCredential(credential)
+                ?.addOnCompleteListener {
+                    handleOnCompleteSignIn(it)
+                    showProgress(false)
+                }
+
+    }
+
+    private fun handleOnCompleteSignIn(task: Task<AuthResult>) {
+        // If sign in fails, display a message to the user. If sign in succeeds
+        // the auth state listener will be notified and logic to handle the
+        // signed in user can be handled in the listener.
+        if (task.isSuccessful) return
+
+        showProgress(false)
+        Toast.makeText(this@LoginActivity, getString(R.string.auth_failed), Toast.LENGTH_SHORT).show()
     }
 }
