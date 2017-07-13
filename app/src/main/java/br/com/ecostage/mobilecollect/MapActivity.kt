a572359a19029a2648757b2f483bfec0863bf9c4
Caio Maia <caaiomaia@gmail.com>
2017-07-13 00:18:01 -0300

    Add buttons to map and scroll with gestures

--- app/src/main/java/br/com/ecostage/mobilecollect/MapActivity.kt
+++ app/src/main/java/br/com/ecostage/mobilecollect/MapActivity.kt
@@ -3,23 +3,38 @@ package br.com.ecostage.mobilecollect
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
-import android.support.v4.app.FragmentActivity
 import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationListener
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 
-class MapActivity : FragmentActivity(), OnMapReadyCallback {
+class MapActivity : AppCompatActivity(),
+        OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     private val MAP_PERMISSION_REQUEST_CODE = 1
+    private lateinit var googleMap: GoogleMap
+    private lateinit var googleApiClient: GoogleApiClient
+    private lateinit var locationRequest: LocationRequest
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
-        if (!canAccessLocation()) requestPermissions()
+        supportActionBar?.title = resources.getString(R.string.map)
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
@@ -27,30 +42,74 @@ class MapActivity : FragmentActivity(), OnMapReadyCallback {
 
     @SuppressLint("MissingPermission")
     override fun onMapReady(map: GoogleMap) {
-        setupUiSettings(map)
-
-        map.mapType = GoogleMap.MAP_TYPE_SATELLITE
+        googleMap = map
+        googleMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
+        setupUiSettings(googleMap)
 
         if (canAccessLocation()) {
+            buildGoogleApiClient()
             map.isMyLocationEnabled = true
         } else {
             requestPermissions()
         }
     }
 
+    @SuppressLint("MissingPermission")
+    override fun onConnected(bundle: Bundle?) {
+//        Log.i(MapActivity::class.simpleName, "Location services connected")
+        locationRequest = LocationRequest()
+        locationRequest.interval = 10000
+        locationRequest.fastestInterval = 10000
+        locationRequest.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+
+        if (canAccessLocation()) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this)
+        }
+    }
+
+    override fun onConnectionSuspended(i: Int) {
+//        Log.i(MapActivity::class.simpleName, "Location services suspended")
+    }
+
+    override fun onConnectionFailed(connectionResult: ConnectionResult) {
+//        Log.e(MapActivity::class.simpleName, "Location services failed to connect")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        if (googleApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this)
+        }
+    }
+
+    override fun onLocationChanged(location: Location?) {
+        if (location != null) {
+            val zoomLevel = 17f
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude, location.longitude), zoomLevel))
+        }
+    }
+
+    @Synchronized private fun buildGoogleApiClient() {
+        googleApiClient = GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build()
+        googleApiClient.connect()
+    }
+
     private fun setupUiSettings(map: GoogleMap) {
         val uiSettings = map.uiSettings
 
-        uiSettings.isScrollGesturesEnabled = false
         uiSettings.isZoomControlsEnabled = true
-        uiSettings.isZoomGesturesEnabled = true
     }
 
-    fun requestPermissions() {
+    private fun requestPermissions() {
         ActivityCompat.requestPermissions(this,
                 arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION),
                 MAP_PERMISSION_REQUEST_CODE)
     }
 
-    fun canAccessLocation(): Boolean = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    private fun canAccessLocation(): Boolean = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
 }
