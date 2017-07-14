92048f4f56a52e7c5ed4378c0afc77a981dcd522
Caio Maia <caaiomaia@gmail.com>
2017-07-14 00:51:29 -0300

    Revert launcher activity to login

--- app/src/main/java/br/com/ecostage/mobilecollect/map/MapPresenterImpl.kt
+++ app/src/main/java/br/com/ecostage/mobilecollect/map/MapPresenterImpl.kt
@@ -1,4 +1,9 @@
 package br.com.ecostage.mobilecollect.map
 
-class MapPresenterImpl(mapView: MapView) : MapPresenter {
+import android.Manifest
+import android.content.pm.PackageManager
+import android.support.v4.content.ContextCompat
+
+class MapPresenterImpl(val mapView: MapView) : MapPresenter {
+
 }
