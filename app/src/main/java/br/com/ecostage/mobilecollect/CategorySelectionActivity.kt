272d72b513c2871edfe34bb4806758dc08fb8a34
Caio Maia <caaiomaia@gmail.com>
2017-07-18 23:42:06 -0300

    Added category selection activity

---
+++ app/src/main/java/br/com/ecostage/mobilecollect/CategorySelectionActivity.kt
@@ -0,0 +1,12 @@
+package br.com.ecostage.mobilecollect
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CategorySelectionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category_selection)
+    }
+}
