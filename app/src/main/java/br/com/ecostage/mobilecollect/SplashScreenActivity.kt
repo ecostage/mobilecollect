2c8ab1406d569cf0d0d804503aadc4e85ae0e993
André Maia <andrenmaia@gmail.com>
2017-08-24 09:57:29 -0300

    Reformulate splashscreen load.

--- app/src/main/java/br/com/ecostage/mobilecollect/SplashScreenActivity.kt
+++ app/src/main/java/br/com/ecostage/mobilecollect/SplashScreenActivity.kt
@@ -1,12 +1,12 @@
 package br.com.ecostage.mobilecollect
 
 import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import br.com.ecostage.mobilecollect.ui.login.LoginActivity
 import com.crashlytics.android.Crashlytics
 import com.google.firebase.auth.FirebaseAuth
 import io.fabric.sdk.android.Fabric
+import org.jetbrains.anko.startActivity
 
 
 /**
@@ -17,29 +17,15 @@ class SplashScreenActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Fabric.with(this, Crashlytics())
-        setContentView(R.layout.activity_splash_screen)
 
         logUser()
 //        FirebaseDatabase.getInstance().setPersistenceEnabled(true)
 
-        val timerThread = object : Thread() {
-            override fun run() {
-                try {
-                    Thread.sleep(3000)
-                } catch (e: InterruptedException) {
-                    e.printStackTrace()
-                } finally {
-                    val intent = Intent(this@SplashScreenActivity, LoginActivity::class.java)
-                    startActivity(intent)
-                }
-            }
-        }
-        timerThread.start()
+        startActivity<LoginActivity>()
+        finish()
     }
 
     private fun logUser() {
-        // TODO: Use the current user's information
-        // You can call any combination of these three methods
         val instance = FirebaseAuth.getInstance()
         val currentUser = instance.currentUser
 
